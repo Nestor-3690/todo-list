15c58ef4c044aa0ccd3233d208645824e74011a4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-05 15:48:55 +0200

    Add Projects function factory in projects.js
    The factory declare an array projects containing all projects
    The factory as a class Project for creating a New Project

---
+++ src/projects.js
@@ -0,0 +1,11 @@
+function Projects() {
+    const projects = [];
+
+    class Project {
+        constructor(title) {
+            this.id = projects.length
+            this.title = title,
+            this.todos = [];
+        }
+    }
+}
