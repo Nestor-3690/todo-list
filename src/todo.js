b80185390212a2097a6959c5875f054766a2fa0f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-05 17:16:06 +0200

    Remove function factory
    The file now only export the Todo class

---
+++ src/todo.js
@@ -0,0 +1,14 @@
+export class Todo {
+        constructor(project, title, description, dueDate, priority) {
+        this.id = project.todos.length,
+        this.title = title,
+        this.description = description,
+        this.dueDate = dueDate,
+        this.priority = priority,
+        this.isDone = false;
+    }
+
+    changeDone() {
+        this.isDone = true ? false : true;
+    }
+}
