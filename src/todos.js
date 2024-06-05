ebb12592631551608b7d4acae5a7e7432b095198
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-05 16:16:58 +0200

    Create Todos function factory
    The factory has a class to create Todo

---
+++ src/todos.js
@@ -0,0 +1,11 @@
+function Todos() {
+    class Todo {
+        constructor(title, description, dueDate, priority) {
+            this.title = title,
+            this.description = description,
+            this.dueDate = dueDate,
+            this.priority = priority,
+            this.isDone = false;
+        }
+    }
+}
