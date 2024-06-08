28d83cec33995147f5e6db5a5e17593b4a4774c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-08 11:01:02 +0200

    Add printAside function, display projects in aside

--- src/domController.js
+++ src/domController.js
@@ -1,12 +1,13 @@
 import { Controller } from "./listController";
 import printList from "./printList";
+import printAside from "./printAside";
 
 export function domController() {
     const list = Controller();
 
     const setList = () => {
         printList(list.projects);
-
+        printAside(list.projects);
     }
 
     setList();
---
+++ src/printAside.js
@@ -0,0 +1,23 @@
+import newButton from "./button";
+
+export default function(projects) {
+    const aside = document.querySelector(".aside-container")
+    for (const project of projects) {
+        const pr = document.createElement("div");
+        pr.classList.add("aside-project")
+        const projectTitle = document.createElement("div");
+        projectTitle.classList.add("aside-project-title");
+        projectTitle.textContent = project.title;
+        pr.appendChild(projectTitle);
+        const projectTodos = document.createElement("div");
+        projectTodos.classList.add("aside-todos");
+        for (const todo of project.todos) {
+            const todoUI = document.createElement("div");
+            todoUI.classList.add("aside-todo");
+            todoUI.textContent = todo.title;
+            projectTodos.appendChild(todoUI);
+        }
+        pr.appendChild(projectTodos);
+        aside.appendChild(pr);
+    }
+}
