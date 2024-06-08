8f2811d3bd3bfa7ff9d693d70bee48ca88295e24
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-08 16:25:08 +0200

    Add ID's for buttons and projects in the dom
    These ID's will ease the implementation of interactive buttons

--- src/button.js
+++ src/button.js
@@ -1,5 +1,8 @@
-export default function(attribute, parent) {
+export default function(attribute, parent, id) {
     const button = document.createElement("button");
     button.classList.add(`${attribute}`);
+    if (id) {
+        button.setAttribute("id", `${id}`);
+    }
     parent.appendChild(button);
 }
