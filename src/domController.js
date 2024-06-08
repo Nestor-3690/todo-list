b21f187ebfab9b82277eea0a931ead512b64c60f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-08 15:15:31 +0200

    Put setDialogs(setInteractivity) in its own module

--- src/domController.js
+++ src/domController.js
@@ -1,6 +1,7 @@
 import { Controller } from "./listController";
 import printList from "./printList";
 import printAside from "./printAside";
+import { setDialogs } from "./setDialogs";
 
 export function domController() {
     const list = Controller();
@@ -8,42 +9,7 @@ export function domController() {
     const setPage = () => {
         printList(list.projects);
         printAside(list.projects);
-        setInteractivity();
-    }
-
-    const setInteractivity = () => {
-        const addProjectBtn = document.querySelector("#ap-button");
-        const projectDialog = document.querySelector("#project-dialog")
-        const submitProjectBtn = document.querySelector("#submit-project");
-
-        const addTodoBtn = document.querySelector("#at-button");
-        const todoDialog = document.querySelector("#todo-dialog");
-        const submitTodoBtn = document.querySelector("#submit-todo");
-
-        addProjectBtn.addEventListener("click", () => projectDialog.showModal());
-        submitProjectBtn.addEventListener("click", (event) => {
-            event.preventDefault();
-            list.addProject(prTitle.value);
-            printList(list.projects);
-            printAside(list.projects);
-        })
-        addTodoBtn.addEventListener("click", () => {
-            todoDialog.showModal();
-            const selectProject = document.querySelector("#todoproject");
-            selectProject.textContent = '';
-            for (const project of list.projects) {
-                const option = document.createElement("option");
-                option.value = `${project.id}`;
-                option.textContent = `${project.title}`;
-                selectProject.appendChild(option);
-            }
-        });
-        submitTodoBtn.addEventListener("click", (event) => {
-            event.preventDefault();
-            list.addTodo(todoproject.value, todotitle.value, tododesc.value, new Date(duedate.value), priority.value)
-            printList(list.projects);
-            printAside(list.projects);
-        });
+        setDialogs(list);
     }
 
     setPage();
---
+++ src/setDialogs.js
@@ -0,0 +1,34 @@
+export function setDialogs(list) {
+    const addProjectBtn = document.querySelector("#ap-button");
+    const projectDialog = document.querySelector("#project-dialog")
+    const submitProjectBtn = document.querySelector("#submit-project");
+
+    const addTodoBtn = document.querySelector("#at-button");
+    const todoDialog = document.querySelector("#todo-dialog");
+    const submitTodoBtn = document.querySelector("#submit-todo");
+
+    addProjectBtn.addEventListener("click", () => projectDialog.showModal());
+    submitProjectBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        list.addProject(prTitle.value);
+        printList(list.projects);
+        printAside(list.projects);
+    })
+    addTodoBtn.addEventListener("click", () => {
+        todoDialog.showModal();
+        const selectProject = document.querySelector("#todoproject");
+        selectProject.textContent = '';
+        for (const project of list.projects) {
+            const option = document.createElement("option");
+            option.value = `${project.id}`;
+            option.textContent = `${project.title}`;
+            selectProject.appendChild(option);
+        }
+    });
+    submitTodoBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        list.addTodo(todoproject.value, todotitle.value, tododesc.value, new Date(duedate.value), priority.value)
+        printList(list.projects);
+        printAside(list.projects);
+    });
+};
