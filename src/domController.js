ef64aa68fd5bf6d431540e6e8d5c371521f6f286
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-08 11:46:51 +0200

    Create dialog to add a new Project

--- src/domController.js
+++ src/domController.js
@@ -8,6 +8,21 @@ export function domController() {
     const setPage = () => {
         printList(list.projects);
         printAside(list.projects);
+        setInteractivity();
+    }
+
+    const setInteractivity = () => {
+        const addProjectBtn = document.querySelector("#ap-button");
+        const projectDialog = document.querySelector("#project-dialog")
+        const submitProjectBtn = document.querySelector("#submit-project");
+
+        addProjectBtn.addEventListener("click", () => projectDialog.showModal());
+        submitProjectBtn.addEventListener("click", (event) => {
+            event.preventDefault();
+            list.addProject(prTitle.value);
+            printList(list.projects);
+            printAside(list.projects);
+        })
     }
 
     setPage();
