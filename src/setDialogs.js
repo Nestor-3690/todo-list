447a9b65c524afbc7525e27618db35eee2836844
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-10 11:51:14 +0200

    Change remove button/function call place
    The buttons are now initialized in setButtons.js
    It allows the buttons to get updated each time a change occurs

--- src/setDialogs.js
+++ src/setDialogs.js
@@ -33,52 +33,10 @@ export function setDialogs(list) {
     });
     submitTodoBtn.addEventListener("click", (event) => {
         event.preventDefault();
-        list.addTodo(todoproject.value, todotitle.value, tododesc.value, new Date(duedate.value), priority.value)
+        list.addTodo(todoproject.value, todotitle.value, tododesc.value, new Date(duedate.value), priority.value);
+        todoDialog.close();
         printList(list.projects);
         printAside(list.projects);
         setButtons(list);
-    });
-
-    const changeTodoBtn = document.querySelectorAll(".change-todo");
-    const changeTodoDialog = document.querySelector("#change-todo-dialog");
-    const submitUpdateBtn = document.querySelector("#submit-change-todo");
-
-    let todoID = 0 ;
-    let projectID = 0;
-
-    changeTodoBtn.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            todoID = btn.parentElement.id;
-            projectID = btn.parentElement.parentElement.parentElement.id;
-
-            changeTodoDialog.showModal();
-            const changeProject = document.querySelector("#changetodoproject");
-            changeProject.textContent = '';
-            for (const project of list.projects) {
-                const option = document.createElement("option");
-                option.value = `${project.id}`;
-                option.textContent = `${project.title}`;
-                changeProject.appendChild(option);
-            }
-            const changeTitle = document.querySelector("#changetodotitle");
-            changeTitle.value = list.projects[projectID].todos[todoID].title;
-
-            const changeDesc = document.querySelector("#changetododesc");
-            changeDesc.value = list.projects[projectID].todos[todoID].description;
-
-            const changePriority = document.querySelector("#changepriority");
-            changePriority.value = list.projects[projectID].todos[todoID].priority;
-        });
-    })
-    submitUpdateBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        if (projectID) {
-            list.changeTodo(projectID, changetodoproject.value, todoID, changetodotitle.value, changetododesc.value, new Date(changeduedate.value), priority.value);
-        }
-        printList(list.projects);
-        printAside(list.projects);
-        setButtons(list);
-        setDialogs(list);
-        changeTodoDialog.close();
     });
 };
