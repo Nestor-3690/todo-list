622ed772fc7c0ac941ed8de37325fafe715a5f47
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-11 09:09:01 +0200

    Add daysDifference function
    The function return the difference between date and today in days

--- src/date.js
+++ src/date.js
@@ -1,13 +1,15 @@
-import { isDate, isFuture, formatDistanceToNowStrict, format } from "date-fns";
+import { isDate, isFuture, formatDistanceToNowStrict, format, differenceInDays } from "date-fns";
 
-function Date() {
+function DateFunctions() {
     const checkDate = (date) => isDate(date) && isFuture(date);
 
     const distanceToNow = (date) =>  formatDistanceToNowStrict(date);
 
     const formatDate = (date) => format(date, 'MM/dd/yyyy HH:mm');
 
-    return { checkDate, distanceToNow, formatDate };
+    const daysDifference = (date) => differenceInDays(date, new Date());
+
+    return { checkDate, distanceToNow, formatDate, daysDifference };
 }
 
-export { Date };
+export { DateFunctions as Date };
